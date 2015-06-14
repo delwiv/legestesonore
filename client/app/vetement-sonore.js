efcfeb60f1262f8fcb5b29d2fa218648598416c1
delwiv <delwiv@gmail.com>
2015-06-14 23:00:02 +0200

    Almost finished corps-dansé

---
+++ client/app/vetement-sonore.js
@@ -0,0 +1,42 @@
+import React    from 'react';
+
+
+var Header = React.createClass({
+    render: function() {
+        return (
+            <div className="header">
+                <header>
+                    <div className="fixed my-topbar">
+                        <nav className="top-bar" data-topbar role="navigation">
+                            <section className="top-bar-section">
+                                <div className="row">
+                                    <div className="presentation large-3 small-3 columns">
+                                        <ul className="left">
+                                            <li><Link to="home">
+                                                <strong>ADÈLE LE MÉNÉLEC--ROBERT</strong><br/>
+                                                Diplôme des métiers darts textiles<br/>
+                                                2015
+                                            </Link></li>
+                                        </ul>
+                                    </div>
+                                    <div className="large-9 small-9 columns topbar-content">
+                                        <ul className="left">
+                                            <li><Link to="cv">CV</Link></li>
+                                        </ul>
+                                        <ul className="right">
+                                            <li><Link to="corps-danse">CORPS-DANSÉ</Link></li>
+                                            <li><Link to="vetement-sonore">VÊTEMENT-SONORE</Link></li>
+                                            <li><Link to="contact">CONTACT</Link></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </section>
+                        </nav>
+                    </div>
+                </header>
+            </div>
+        );
+    }
+});
+
+export default Header;
